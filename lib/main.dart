2ac85188484836eaa67e21c1ef65d113d3bd6230
PeachBlack-Alba <albatdr@gmail.com>
2021-02-10 12:07:13 +0100

    trying positioning

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:onBoarding/components/OnboardingScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Onboarding UI',
+      debugShowCheckedModeBanner: false,
+      home: OnboardingScreen(),
+    );
+  }
+}
---
+++ lib/ui/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final kTitleStyle = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Open Sans Condensed',
+  fontSize: 26.0,
+  height: 1.5,
+);
+
+final kSubtitleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 18.0,
+  height: 1.2,
+);
